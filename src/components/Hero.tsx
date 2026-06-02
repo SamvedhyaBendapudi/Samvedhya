c77ef333a24ff6e45fd129e7a09bf5809639f231
Samvedhya Bendapudi <105159635+SamvedhyaBendapudi@users.noreply.github.com>
2026-06-02 13:44:24 -0700

    Fix formatting of quote in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -119,7 +119,7 @@ export default function Hero() {
             className="mt-8 max-w-3xl text-xl leading-relaxed text-white md:text-2xl"
             style={{ fontFamily: "Georgia, Times New Roman, serif", fontStyle: "italic" }}
           >
-            &quot;Code is like humor. When you have to explain it, it's bad. Fortunately, explaining things is one of my strengths."&quot;
+            &quot;Code is like humor. When you have to explain it, it's bad. Fortunately, explaining things is one of my strengths.&quot;
           </motion.p>
 
           <motion.div
